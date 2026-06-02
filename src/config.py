2dbcc60fb001886acf0b89f7cf750fe7454788d9
jjs <jaeseung716@gmail.com>
2026-06-02 23:21:25 +0900

    fix: auto-update tunnel url (2026-06-02 23:21:25)

--- src/config.py
+++ src/config.py
@@ -1,1 +1,2 @@
-BUTLER_API_URL = "https://florist-gel-president-domestic.trycloudflare.com"
+BUTLER_API_URL = "https://showers-themselves-wisdom-walker.trycloudflare.com"
+BUTLER_API_TOKEN = "butler_v3_secret_2026"
