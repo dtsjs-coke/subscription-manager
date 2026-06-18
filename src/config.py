48c881632281a06c946848d95cdc85f33d575ca5
jjs <jaeseung716@gmail.com>
2026-06-19 01:25:40 +0900

    fix: auto-update tunnel url (2026-06-19 01:25:40)

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
-BUTLER_API_URL = "https://lung-bolt-committed-finite.trycloudflare.com"
+BUTLER_API_URL = "https://orlando-veterinary-tucson-introduction.trycloudflare.com"
 BUTLER_API_TOKEN = "butler_v3_secret_2026"
