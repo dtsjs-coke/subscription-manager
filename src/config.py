fd455be08e776071ecf38cd8c276953b71efda81
jjs <jaeseung716@gmail.com>
2026-07-04 22:54:20 +0900

    fix: auto-update tunnel url (2026-07-04 22:54:20)

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
-BUTLER_API_URL = "https://orlando-veterinary-tucson-introduction.trycloudflare.com"
+BUTLER_API_URL = "https://toxic-frequencies-expectations-comparative.trycloudflare.com"
 BUTLER_API_TOKEN = "butler_v3_secret_2026"
