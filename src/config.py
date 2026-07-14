188022300d9006fe592031469245661c6b005c38
jjs <jaeseung716@gmail.com>
2026-07-14 19:52:29 +0900

    fix: auto-update tunnel url (2026-07-14 19:52:29)

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
-BUTLER_API_URL = "https://simpson-phpbb-cultural-nine.trycloudflare.com"
+BUTLER_API_URL = "https://cursor-tower-alfred-paris.trycloudflare.com"
 BUTLER_API_TOKEN = "butler_v3_secret_2026"
