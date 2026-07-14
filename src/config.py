1cfc55a7260ad7a6ca7519dace195622bacb9da9
jjs <jaeseung716@gmail.com>
2026-07-14 19:55:46 +0900

    fix: auto-update tunnel url (2026-07-14 19:55:46)

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
-BUTLER_API_URL = "https://doll-position-wto-promoted.trycloudflare.com"
+BUTLER_API_URL = "https://greatest-amend-eternal-toilet.trycloudflare.com"
 BUTLER_API_TOKEN = "butler_v3_secret_2026"
