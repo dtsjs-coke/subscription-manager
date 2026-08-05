be1acc6e09a2e713fb96619d6cbd8b01dc335e4c
jjs <jaeseung716@gmail.com>
2026-08-05 22:40:20 +0900

    fix: auto-update tunnel url (2026-08-05 22:40:20)

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
-BUTLER_API_URL = "https://bye-please-illinois-farming.trycloudflare.com"
+BUTLER_API_URL = "https://framework-carrying-prospects-anybody.trycloudflare.com"
 BUTLER_API_TOKEN = "butler_v3_secret_2026"
