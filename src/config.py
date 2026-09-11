b1eac73e31fd639957e0f69af8892ca8fe101847
jjs <jaeseung716@gmail.com>
2026-09-11 18:20:43 +0900

    fix: auto-update tunnel url (2026-09-11 18:20:43)

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
-BUTLER_API_URL = "https://editorial-representing-endorsed-halo.trycloudflare.com"
+BUTLER_API_URL = "https://employers-timeline-record-agencies.trycloudflare.com"
 BUTLER_API_TOKEN = "butler_v3_secret_2026"
